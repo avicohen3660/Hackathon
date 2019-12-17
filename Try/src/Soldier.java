0da28562ec0952c23fd8b24c702e17de04421556
Itamar <itamarshubin>
2019-12-18 00:24:19 +0200

    update the soldier class

--- Try/src/Soldier.java
+++ Try/src/Soldier.java
@@ -3,9 +3,31 @@ public class Soldier {
         NormalSoldier,
         Archer,
         Defender
-    };
+    }
+    type t;
+    boolean isRight;
     int life;
     boolean isDead;
     boolean isAttack;
     int position;
+
+    public Soldier(int type,boolean isRight){
+        switch (type){
+            case 1:
+                this.t= Soldier.type.NormalSoldier;
+                break;
+            case 2:
+                this.t= Soldier.type.Archer;
+                break;
+            case 3:
+                this.t= Soldier.type.Defender;
+                break;
+
+        }
+        this.isRight=isRight;
+
+    }
+
+
+
 }
